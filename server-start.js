38a6277a4ab5e1ef70872930a7d631323e65c908
Gilmart Quentin <quentin.gilmart@student.vinci.be>
2018-06-27 04:42:40 +0200

    start:prod

--- server-start.js
+++ server-start.js
@@ -1,5 +1,5 @@
 process.env.NODE_ENV= process.env.NODE_ENV || 'development'
-process.env.SERVER_PORT= process.env.SERVER_PORT || '4040'
+process.env.SERVER_PORT= process.env.PORT || '4040'
 process.env.JWT_SECRET='0a6b944d-d2fb-46fc-a85e-0295c986cd9f'
 process.env.MONGO_HOST= process.env.MONGODB_URI || 'mongodb://localhost:27017/mern-starter'
 process.env.MONGO_PORT='35574'
